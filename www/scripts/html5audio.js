54266610e02ea5ae85d68e749e52db3bd507c686
tommy-carlos williams <tommy@devgeeks.org>
2012-03-11 12:20:32 +1100

    Initial import

---
+++ www/scripts/html5audio.js
@@ -0,0 +1,101 @@
+
+
+
+var progressTimer;
+
+var playButton;
+var stopButton;
+var activityIndicator;
+var textPosition;
+
+function onError(error) 
+{
+	console.log(error.message);
+}
+
+function onConfirmRetry(button) {
+	if (button == 1) {
+		html5audio.play();
+	}
+}
+
+function pad2(number) {
+	return (number < 10 ? '0' : '') + number
+}
+
+var myaudio = new Audio('http://stream.4zzzfm.org.au:789/');
+var isPlaying = false;
+var readyStateInterval = null;
+
+var html5audio = {
+	play: function()
+	{
+		isPlaying = true;
+		myaudio.play();
+	
+		readyStateInterval = setInterval(function(){
+			 if (myaudio.readyState <= 2) {
+				 playButton.style.display = 'none';
+				 activityIndicator.style.display = 'block';
+				 textPosition.innerHTML = 'loading...';
+			 }
+		},1000);
+		myaudio.addEventListener("timeupdate", function() {
+			 var s = parseInt(myaudio.currentTime % 60);
+			 var m = parseInt((myaudio.currentTime / 60) % 60);
+			 var h = parseInt(((myaudio.currentTime / 60) / 60) % 60);
+			 if (isPlaying && myaudio.currentTime > 0) {
+				 textPosition.innerHTML = pad2(h) + ':' + pad2(m) + ':' + pad2(s);
+			 }
+		}, false);
+		myaudio.addEventListener("error", function() {
+			 console.log('myaudio ERROR');
+		}, false);
+		myaudio.addEventListener("canplay", function() {
+			 console.log('myaudio CAN PLAY');
+		}, false);
+		myaudio.addEventListener("waiting", function() {
+			 //console.log('myaudio WAITING');
+			 isPlaying = false;
+			 playButton.style.display = 'none';
+			 stopButton.style.display = 'none';
+			 activityIndicator.style.display = 'block';
+		}, false);
+		myaudio.addEventListener("playing", function() {
+			 isPlaying = true;
+			 playButton.style.display = 'none';
+			 activityIndicator.style.display = 'none';
+			 stopButton.style.display = 'block';
+		}, false);
+		myaudio.addEventListener("ended", function() {
+			 //console.log('myaudio ENDED');
+			 html5audio.stop();
+			 // navigator.notification.alert('Streaming failed. Possibly due to a network error.', null, 'Stream error', 'OK');
+			 navigator.notification.confirm(
+				'Streaming failed. Possibly due to a network error.', // message
+				onConfirmRetry,	// callback to invoke with index of button pressed
+				'Stream error',	// title
+				'Retry,OK'		// buttonLabels
+			 );
+		}, false);
+	},
+	pause: function() {
+		isPlaying = false;
+		clearInterval(readyStateInterval);
+		myaudio.pause();
+		stopButton.style.display = 'none';
+		activityIndicator.style.display = 'none';
+		playButton.style.display = 'block';
+	},
+	stop: function() {
+		isPlaying = false;
+		clearInterval(readyStateInterval);
+		myaudio.pause();
+		stopButton.style.display = 'none';
+		activityIndicator.style.display = 'none';
+		playButton.style.display = 'block';
+		myaudio = null;
+		myaudio = new Audio('http://stream.4zzzfm.org.au:789/');
+		textPosition.innerHTML = '';
+	}
+};
